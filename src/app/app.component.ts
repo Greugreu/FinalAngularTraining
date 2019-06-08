d2ba55af03a08e0e6b728c5627364f7193b2dd35
Greugreu <fenris.fitz@hotmail.fr>
2019-06-08 17:39:13 +0200

    signIn component signOut compnent signUp component bookform singlebook view booklist
    database writing
    database get
    database set
    
    authguard
    canActivate routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FinalAngular';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyCVxE98bNn0Ca0uKtJOpjtOYPepdXCDm2A",
+      authDomain: "finalangularp.firebaseapp.com",
+      databaseURL: "https://finalangularp.firebaseio.com",
+      projectId: "finalangularp",
+      storageBucket: "",
+      messagingSenderId: "570716373928",
+      appId: "1:570716373928:web:771361b04a2898c1"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
